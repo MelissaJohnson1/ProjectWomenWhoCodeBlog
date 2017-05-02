c2b56d1e526b3cc1698be993c09885fc1aeaddc3
MelissaJohnson1 <Johnson.Melissa.100@gmail.com>
2017-05-02 09:56:26 -0500

    Add files via upload

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,174 @@
+﻿using QueensOfCodeProject.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QueensOfCodeProject.UI.Controllers
+{
+    public class AdminController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpGet]
+        public ActionResult ListPosts()
+        {
+            PostRepo reap = new PostRepo();
+            var model = reap.GetAllPosts();
+
+            return View(model);
+
+        }
+
+        [HttpGet]
+        public ActionResult DeletePost(int id)
+        {
+            PostRepo reap = new PostRepo();
+            var model = reap.GetPostById(id);
+            return View(model);
+
+        }
+
+        [HttpPost]
+        public ActionResult DeletePost(Post blog)
+        {
+            PostRepo reap = new PostRepo();
+            reap.Delete(blog.Id);
+            return RedirectToAction("ListPosts");
+        }
+
+        [HttpGet]
+        public ActionResult CreatePost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreatePost(Post blog)
+        {
+
+            if (ModelState.IsValid)
+            {
+               PostRepo pr = new PostRepo();
+
+                var fabpost = new Post();
+                fabpost.Title = blog.Title;
+                fabpost.CreationDate = blog.CreationDate;
+                fabpost.PostText = blog.PostText;
+                fabpost.ExpirationDate = blog.ExpirationDate;
+                pr.Add(fabpost);
+                return RedirectToAction("ListPosts");
+
+            }
+            return View(blog);
+
+        }
+        [HttpGet]
+        public ActionResult EditPost(int id)
+        {
+
+            PostRepo reap = new PostRepo();
+            var model = reap.GetPostById(id);
+            return View(model);
+           
+        }
+
+        [HttpPost]
+        public ActionResult EditPost(Post blog)
+        {
+            if (ModelState.IsValid)
+            {
+                PostRepo pr = new PostRepo();
+
+                pr.Edit(blog);
+                return RedirectToAction("ListPosts");
+
+            }
+            return View(blog);
+
+        }
+
+        [HttpGet]
+        public ActionResult ListPages()
+        {
+            PageRepo reap = new PageRepo();
+            var model = reap.GetAllPages();
+
+            return View(model);
+
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            PageRepo reap = new PageRepo();
+            var model = reap.GetPageById(id);
+            return View(model);
+
+        }
+
+        [HttpPost]
+        public ActionResult DeletePage(Page webpage)
+        {
+            PageRepo reap = new PageRepo();
+            reap.Delete(webpage.Id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult CreatePage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreatePage(Page webpage)
+        {
+
+            if (ModelState.IsValid)
+            {
+                PageRepo pr = new PageRepo();
+
+                var fabpost = new Page();
+                fabpost.Title = webpage.Title;
+                fabpost.PostText = webpage.PostText;
+                
+                pr.Add(fabpost);
+                return RedirectToAction("Index");
+
+            }
+            return View(webpage);
+
+        }
+        [HttpGet]
+        public ActionResult EditPage(int id)
+        {
+
+            PageRepo reap = new PageRepo();
+            var model = reap.GetPageById(id);
+            return View(model);
+
+        }
+
+        [HttpPost]
+        public ActionResult EditPage(Page webpage)
+        {
+            if (ModelState.IsValid)
+            {
+                PageRepo pr = new PageRepo();
+
+                pr.Edit(webpage);
+                return RedirectToAction("Index");
+
+            }
+            return View(webpage);
+
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using QueensOfCodeProject.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QueensOfCodeProject.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            PageRepo pg = new PageRepo();
+            PostRepo pr = new PostRepo();
+            var model = new DisplayPagePostViewModel();
+            model.Posts = pr.GetAllPosts();
+            model.Pages = pg.GetAllPages();
+            
+            return View(model);
+            
+        }
+
+        [HttpGet]
+        public ActionResult GetPostDetail(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                PostRepo pr = new PostRepo();
+                var post = pr.GetPostById(id);
+                return View(post);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            var model = new LoginViewModel();
+
+            ViewBag.ReturnUrl = Url.Action("ListPosts", "Admin");
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var userManager = HttpContext.GetOwinContext().GetUserManager<UserManager<AppUser>>();
+            var authManager = HttpContext.GetOwinContext().Authentication;
+
+            // attempt to load the user with this password
+            AppUser user = userManager.Find(model.UserName, model.Password);
+
+            // user will be null if the password or user name is bad
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Invalid username or password");
+
+                return View(model);
+            }
+            else
+            {
+                // successful login, set up their cookies and send them on their way
+                var identity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                authManager.SignIn(new AuthenticationProperties { IsPersistent = model.RememberMe }, identity);
+
+                if (!string.IsNullOrEmpty(returnUrl))
+                    return Redirect(returnUrl);
+                else
+                    return RedirectToAction("Index", "Admin");
+            }
+
+
+        }
+
+        [HttpGet]
+        public ActionResult SeeAllPosts()
+        {
+            PostRepo reap = new PostRepo();
+            var model = reap.GetAllPosts();
+
+            return View(model);
+
+        }
+    }
+}
