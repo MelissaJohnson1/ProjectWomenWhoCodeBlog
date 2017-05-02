20f273da2220fe37ac5293ddb16b910b0c609606
MelissaJohnson1 <Johnson.Melissa.100@gmail.com>
2017-05-02 10:03:25 -0500

    Add files via upload

---
+++ App_Start/IdentityConfig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using QueensOfCodeProject.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QueensOfCodeProject.UI.App_Start
+{
+    
+        public class IdentityConfig
+        {
+            public void Configuration(IAppBuilder app)
+            {
+                app.CreatePerOwinContext(() => new QueensOfCodeDbContext());
+
+                app.CreatePerOwinContext<UserManager<AppUser>>((options, context) =>
+                    new UserManager<AppUser>(
+                        new UserStore<AppUser>(context.Get<QueensOfCodeDbContext>())));
+
+                app.CreatePerOwinContext<RoleManager<AppRole>>((options, context) =>
+                    new RoleManager<AppRole>(
+                        new RoleStore<AppRole>(context.Get<QueensOfCodeDbContext>())));
+
+                app.UseCookieAuthentication(new CookieAuthenticationOptions
+                {
+                    AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                    LoginPath = new PathString("/Home/Login"),
+                });
+            }
+        }
+ 
+}
